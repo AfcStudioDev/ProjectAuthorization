2fbd3fbd83fee199520438f3cf45a48a26f88836
CvCvCvv <decomen123@gmail.com>
2024-09-12 16:26:21 +0500

    обновление фронта

--- company/src/app/home/home.component.ts
+++ company/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Input, Component } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 import { FormsModule, Validators } from '@angular/forms';
+declare let YooMoneyCheckoutWidget: any;
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,9 @@ import { FormsModule, Validators } from '@angular/forms';
   styleUrl: './home.component.css'
 })
 export class HomeComponent{
-  constructor(private router: Router){}
+  constructor(private router: Router){
+
+  }
   login: string = "tg";
 
   getLogin(){
@@ -19,4 +22,59 @@ export class HomeComponent{
   OnLoginButtonClick(){
     this.router.navigate(["/login"]);
   }
+  ngAfterViewInit()
+  {
+    alert("Телеграм: https://t.me/NeedMoreRamonaFlowers");
+  }
+  GoPay(type: number) {
+    var host = window.location.host;
+    console.log(host);
+
+    var checkout = new YooMoneyCheckoutWidget({
+      confirmation_token: "response.confirmation.confirmation_token",
+     customization: {
+        modal: true
+      },
+      error_callback: (error: any) => {
+        console.log(error);
+      }
+    });
+    
+    checkout.render()
+       .then(() => {
+
+       });
+
+//     checkout.on('success', () => {
+//       alert("Оплата успешно произведена!");
+//       checkout.destroy();
+//       this.licenseService.ConfirmLicenseCreate(response.id, type.toString()).subscribe({ 
+//         next: (response) => {
+//           if(!response)
+//           {
+//             alert("Произошла ошибка при выдаче лицензии!");
+//           }
+//         }
+//        });
+//       this.router.navigate(['/projects']);
+//     });
+
+//     checkout.on('fail', () => {
+//       alert("Оплата прошла неудачно, попробуйте снова");
+
+//       checkout.destroy();
+//     });
+
+//     checkout.render()
+//       .then(() => {
+
+//       });
+//   },
+//   error: (error)=>{
+//     alert("У вас уже имеется лицензия!");
+//     this.router.navigate(['/profile']);
+//   }
+// });
+}
+  
 }
