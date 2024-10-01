7fe14be14303d5b40665604ec51fce8cf4709dc6
Polskih Ivan <polskikh.ioan@gmail.com>
2024-10-01 11:20:36 +0500

    изменены типы данных в таблице LicenseType

--- Authorization.Application/Domain/Entities/LicenseType.cs
+++ Authorization.Application/Domain/Entities/LicenseType.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+using Microsoft.EntityFrameworkCore;
+
 namespace Authorization.Application.Domain.Entities
 {
     /// <summary>
@@ -8,10 +10,11 @@ namespace Authorization.Application.Domain.Entities
     public class LicenseType
     {
         [Key]
-        public uint Id { get; set; }
+        public UInt16 Id { get; set; }
         [Required]
         public string Name { get; set; } = null!;
         [Required]
+        [Precision(8,2)]
         public decimal Price { get; set; }
         [Required]
         public UInt16 Duration { get; set; }
