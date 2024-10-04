2a12e616f87e2fe6637ebadb4a12b2c66f781ec3
CvCvCvv <decomen123@gmail.com>
2024-10-04 11:51:43 +0500

    добавлены миграции

--- Authorization.Infrastructure.Database/ServiceCollection.cs
+++ Authorization.Infrastructure.Database/ServiceCollection.cs
@@ -2,7 +2,7 @@
 using Authorization.Application.Domain.Entities;
 using Authorization.Infrastructure.Database.Contexts;
 using Authorization.Infrastructure.Database.Repositories;
-
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -22,6 +22,20 @@ namespace Authorization.Infrastructure.Database
             _ = services.AddTransient<IRepository<User>, UserRepository>();
             _ = services.AddTransient<IRepository<Device>, DeviceRepository>();
             _ = services.AddTransient<IRepository<LicenseType>, LicenseTypeRepository>();
+        }
+
+        public static void AddMigrations(this WebApplication application)
+        {
+            using (var scope = application.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<AuthorizationServiceContext>();
+                if (context.Database.GetPendingMigrations().Any())
+                {
+                    context.Database.Migrate();
+                }
+            }
         }
     }
 }
